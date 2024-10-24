0ddbf403d50d45a0d98ea351bb4a13460fcd73cb
Rennam Victor Cabral de Faria <98751401+RennamFaria@users.noreply.github.com>
2024-10-23 21:46:13 -0300

    A

---
+++ CG/Atividades/public/1/main.js
@@ -0,0 +1,203 @@
+function main(draw) {
+    const canvas = document.querySelector("#c");
+    const gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        throw new Error('WebGL not supported');
+    }
+
+    canvas.addEventListener("mousedown", mouseDown, false);
+
+    function mouseDown(event) {
+        console.log(event.screenX);
+        console.log(event.screenY);
+    }
+
+    var vertexShader = createVertexShader(gl);
+    var fragmentShader = createFragmentShader(gl);
+    
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    gl.useProgram(program);
+
+    const positionBuffer = gl.createBuffer();
+    const colorBuffer = gl.createBuffer();
+
+    const positionLocation = gl.getAttribLocation(program, `position`);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    const colorLocation = gl.getAttribLocation(program, `color`);
+    gl.enableVertexAttribArray(colorLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+    gl.clearColor(0.0, 0.0, 0.0, 0.0); // Makes the WebGL background transparent
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    n = 30;
+
+    switch(draw){
+        case 1:         //drawCar
+            //wheels
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); 
+            setCircleVertices(gl, n, 0.5, 1, 1);
+            gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+            setCircleColor(gl, n, [Math.random(), Math.random(), Math.random()]);
+            gl.drawArrays(gl.TRIANGLES, 0, 3 * n);
+        case 2:         //drawFlower
+
+        case 3:         //drawClown
+
+    }
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // setRectangleVertices(gl, -0.75, -0.75, 0.5, 0.5);
+    // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // setRectangleColor(gl, [Math.random(), Math.random(), Math.random()]);
+    // gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // setRectangleVertices(gl, 0.25, 0.25, 0.5, 0.5);
+    // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // setRectangleColor(gl, [Math.random(), Math.random(), Math.random()]);
+    // gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // setRectangleVertices(gl, 0.25, -0.75, 0.5, 0.5);
+    // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // setRectangleColor(gl, [Math.random(), Math.random(), Math.random()]);
+    // gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // setRectangleVertices(gl, -0.75, 0.25, 0.5, 0.5);
+    // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // setRectangleColor(gl, [Math.random(), Math.random(), Math.random()]);
+    // gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    // n = 30;
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // setCircleVertices(gl, n, 0.5);
+    // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    // setCircleColor(gl, n, [Math.random(), Math.random(), Math.random()]);
+    // gl.drawArrays(gl.TRIANGLES, 0, 3 * n);
+}
+
+function createVertexShader(gl) {
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);     // create vertex shader
+    gl.shaderSource(vertexShader, `
+    attribute vec2 position;
+    attribute vec3 color;
+    varying vec3 vColor;
+
+    void main() {
+        gl_Position = vec4(position,0.0,1.0);
+        vColor = color;
+    }
+    `);
+    gl.compileShader(vertexShader);
+
+    const sucess = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS);
+    if(sucess){
+        return vertexShader;
+    }
+
+    console.log(gl.getShaderInfoLog(vertexShader));
+    gl.deleteShader(vertexShader);
+}
+
+function createFragmentShader(gl) {
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); // create fragment shader
+    gl.shaderSource(fragmentShader, `
+    precision mediump float;
+
+    varying vec3 vColor;        
+
+    void main() {
+        gl_FragColor = vec4(vColor,1.0);
+    }
+    `);
+    gl.compileShader(fragmentShader);
+
+    const sucess = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
+    if(sucess){
+        return fragmentShader;
+    }
+
+    console.log(gl.getShaderInfoLog(fragmentShader));
+    gl.deleteShader(fragmentShader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function setRectangleVertices(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]), gl.STATIC_DRAW);
+}
+
+function setRectangleColor(gl, color) {
+    colorData = [];
+    for (let triangle = 0; triangle < 2; triangle++) {
+        for (let vertex = 0; vertex < 3; vertex++)
+            colorData.push(...color);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+}
+
+function setCircleVertices(gl, n, radius, x, y) {
+    let center = [x, y];
+    let vertexData = [];
+    for (let i = 0; i < n; i++) {
+        vertexData.push(...center);
+        vertexData.push(...[radius * Math.cos(i * (2 * Math.PI) / n), radius * Math.sin(i * (2 * Math.PI) / n)]);
+        vertexData.push(...[radius * Math.cos((i + 1) * (2 * Math.PI) / n), radius * Math.sin((i + 1) * (2 * Math.PI) / n)]);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+}
+
+function setCircleColor(gl, n, color) {
+    colorData = [];
+    for (let triangle = 0; triangle < n; triangle++) {
+        for (let vertex = 0; vertex < 3; vertex++)
+            colorData.push(...color);
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+}
+
+function drawCar() {
+    console.log('Drawing car');
+    main(1);
+}
+
+function drawFlower() {
+    console.log('Drawing flower');
+    main(2);
+}
+
+function drawClown() {
+    console.log('Drawing clown');
+  main(3);
+}
